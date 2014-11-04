e6c0b64b289676ec93fcab9869e6b3a543234d3f
lazcatluc <lazcatluc@gmail.com>
2014-11-04 23:02:55 +0200

    added email validation in form"

--- src/test/java/ro/contezi/novote/repository/RegistrationRepositoryTest.java
+++ src/test/java/ro/contezi/novote/repository/RegistrationRepositoryTest.java
@@ -1,9 +1,12 @@
 package ro.contezi.novote.repository;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import ro.contezi.novote.model.Registration;
+import ro.contezi.novote.model.User;
 
 public class RegistrationRepositoryTest {
 	
@@ -17,5 +20,7 @@ public class RegistrationRepositoryTest {
 	@Test
 	public void hasEmailAfterAddingRegistration() throws Exception {
 		registrationRepository.saveRegistration(Registration.SOMETHING);
+		
+		assertTrue(registrationRepository.hasEmailRegistration(User.SOMEONE.getEmail()));
 	}
 }
